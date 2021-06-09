1c64b93160294e4bfcfdcd7dfab5cb3d6849e11c
Christine Antonio <christinejm.antonio@gmail.com>
2021-06-09 15:21:14 -0700

    fix postgresql errors in seed files

--- seeds/seed.earlybird_books.sql
+++ seeds/seed.earlybird_books.sql
@@ -1,6 +1,6 @@
-INSERT INTO earlybird_books (book_id, book_title, book_author, book_genre, book_date_started, book_date_modified)
+INSERT INTO earlybird_books (book_id, book_title, book_author, book_genre, book_date_started, book_finished, book_date_modified)
 VALUES 
-('0a33587e-c4a8-11eb-8529-0242ac130003', 'The Body Keeps the Score', 'Self-help', '2017-03-15', FALSE, '2021-03-15T0:00'),
+('0a33587e-c4a8-11eb-8529-0242ac130003', 'The Body Keeps the Score', 'Bessel Van der Kolk MD', 'Self-help', '2017-03-15', FALSE, '2021-03-15T0:00'),
 ('a05abb3a-c4a8-11eb-8529-0242ac130003', 'The Life Changing Magic of Tidying Up', 'Marie Kondo', 'Self-help', '2021-01-30', TRUE, '2021-01-30T00:00'),
 ('0d7c1700-c4a8-11eb-8529-0242ac130003', 'Milk and Honey', 'Rupi Kaur', 'Poetry', '2020-08-20', TRUE, '2020-08-20T00:00'),
 ('1038017a-c4a8-11eb-8529-0242ac130003', 'The Wind Up Bird Chronicle', 'Haruki Murakami', 'Psychological Fiction', '2018-12-10', TRUE, '2018-12-10T00:00')
