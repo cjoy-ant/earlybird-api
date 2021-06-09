6edcacbc0b3c64c4bd99f5cd8bd0d8f8420691b5
Christine Antonio <christinejm.antonio@gmail.com>
2021-06-09 16:00:06 -0700

    install knex, xss, config to use d, config to use local or vercel client

--- src/config.js
+++ src/config.js
@@ -1,4 +1,8 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || "development",
+  CLIENT_ORIGIN="https://earlybird-client.vercel.app",
+  LOCAL_CLIENT: process.env.LOCAL_CLIENT,
+  DATABASE_URL: process.env.DATABASE_URL,
+  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL,
 };
