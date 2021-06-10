0426ffb12dcabcfcc732efedaa2c30d64e24ef35
Christine Antonio <christinejm.antonio@gmail.com>
2021-06-09 18:25:05 -0700

    GET /:entry_id

--- src/entries/entries-router.js
+++ src/entries/entries-router.js
@@ -71,5 +71,34 @@ entryRouter
       })
       .catch(next);
   });
+entryRouter
+  .route("/:entry_id")
+  .all((req, res, next) => {
+    const knex = req.app.get("db");
+    EntriesService.getById(knex, req.params.entry_id)
+      .then((entry) => {
+        if (!entry) {
+          return res.status(404).json({
+            error: { message: `Entry not found` },
+          });
+        }
+        res.entry = entry;
+        next();
+      })
+      .catch(next);
+  })
+  .get((req, res, next) => {
+    res.json({
+      entry_id: res.entry.entry_id,
+      entry_book_id: res.entry.entry_book_id,
+      entry_title: xss(res.entry.bentry_title),
+      entry_category: xss(res.entry.entry_category),
+      entry_chapters: xss(res.entry.entry_chapters),
+      entry_pages: xss(res.entry.entry_pages),
+      entry_quote: xss(res.entry.entry_quote),
+      entry_notes: xss(res.entry.entry_notes),
+      entry_date_modified: res.entry.entry_date_modified,
+    });
+  });
 
 module.exports = entryRouter;
