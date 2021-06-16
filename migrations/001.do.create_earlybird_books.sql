f6c835c08ce682203e983e7f3b57cdaa1e9a34fb
Christine Antonio <christinejm.antonio@gmail.com>
2021-06-15 17:25:00 -0700

    revert timestamptz to text

--- migrations/001.do.create_earlybird_books.sql
+++ migrations/001.do.create_earlybird_books.sql
@@ -4,7 +4,7 @@ CREATE TABLE earlybird_books (
   book_title TEXT NOT NULL,
   book_author TEXT NOT NULL,
   book_genre TEXT NOT NULL,
-  book_date_started TIMESTAMPTZ NOT NULL,
+  book_date_started TEXT NOT NULL,
   book_finished BOOLEAN DEFAULT FALSE,
   book_date_modified TIMESTAMPTZ DEFAULT NOW()
 );
